b558642c7e1508030caeea29f11f00400be18567
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-09-14 14:55:23 +0530

    [Saurabh]Modified: Start with Toss

--- TicTacToe.java
+++ TicTacToe.java
@@ -5,22 +5,39 @@ class TicTacToe
         static final int START_POSITION = 0;
         static char[] board =  {'-','-','-','-','-','-','-','-','-'};
         static int count;
-
+        static char Player;
+        static char Computer;
+        static final int P_NUM = 1;
+        static Scanner scan = new Scanner(System.in);
+        static Random random = new Random();
         public void resetBoard()
         {
-                 int position;
+		System.out.println("********************** Wellcome To Tic Tac Toe **********************");
+               	int position;
                 for(position = START_POSITION ; position < END_POSITION ; position++ )
                 {
                         board[position] = '-';
                 }
         }
+        public int toss()
+        {
 
-
+                int toss = random.nextInt(2);
+                if( toss == P_NUM)
+                {
+                        System.out.println("You Won The Toss");
+                }
+                else
+                {
+                        System.out.println("Computer Won The Toss");
+                }
+                return toss;
+        }
 
         public static void main(String[] args)
         {
                 TicTacToe Game = new TicTacToe();
                 Game.resetBoard();
-
+                Game.toss();
         }
 }
