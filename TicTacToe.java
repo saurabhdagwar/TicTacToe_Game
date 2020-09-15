a1b0bdec268613f73ad40bc9e33a7b2a4bf02836
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-09-15 21:36:11 +0530

    [Saurabh]Refactored: Added if condition for player

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,211 +1,257 @@
-import java.util.*; class TicTacToe {
+import java.util.*;
+class TicTacToe
+{
 	static final int END_POSITION = 9;
-	static final int START_POSITION = 0;
+	static final int START_POSITION = 1;
 	static char[] board =  {'-','-','-','-','-','-','-','-','-','-'};
 	static int count;
-	static int position;
-	static char Player_Symbol;
-	static char Computer_Symbol;
+	static final int ONE = 1, TWO = 2, THREE = 3, FOUR = 4, FIVE = 5, SIX = 6, SEVEN = 7, EIGHT = 8, NINE = 9;
 	static final int P_NUM = 1;
-	static final int C_NUM = 2;
-	static Scanner scan = new Scanner(System.in);
+        static final int C_NUM = 2;
+	static char Player_Symbol;
+        static char Computer_Symbol;
 	static Random random = new Random();
-	static char X = 'X';
-	static char O = 'O';
+	static Scanner scan = new Scanner(System.in);
+	static final char X = 'X';
+	static final char O = 'O';
 	static int toss;
-	static int AllowComp = 1 ;
+	static int AllowComp = 2;
+	static int position = 1 ;
+	static int tiecount = 0;
 	static int win = 0;
-	static final int ONE = 0, TWO = 1, THREE = 2, FOUR = 3, FIVE = 4, SIX = 5, SEVEN = 6, EIGHT = 7, NINE = 8;
+// Program start
 	public void resetBoard()
 	{
 		System.out.println("********************** Wellcome To Tic Tac Toe **********************");
+		System.out.println("Resetting Board");
+
 		 int position;
 		for(position = START_POSITION ; position <= END_POSITION ; position++ )
 		{
 			board[position] = '-';
 		}
 	}
+       
+	 public void DisplayBoard()
+        {
+                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                System.out.println("    |-----|-----|-----| ");
+                System.out.println("    |  "+board[ONE]+"  |  "+board[TWO]+"  |  "+board[THREE]+"  |");
+                System.out.println("    |-----|-----|-----| ");
+                System.out.println("    |  "+board[FOUR]+"  |  "+board[FIVE]+"  |  "+board[SIX]+"  |");
+                System.out.println("    |-----|-----|-----| ");
+                System.out.println("    |  "+board[SEVEN]+"  |  "+board[EIGHT]+"  |  "+board[NINE]+"  |");
+                System.out.println("    |-----|-----|-----| ");
+                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        }
 	public void tossRandom()
-	{
+        {
 
-		toss = random.nextInt(2);
-		if( toss == P_NUM)
-		{
-			System.out.println("You Won The Toss");
-		}
-		else
-		{
-			System.out.println("Computer Won The Toss");
-		}
+                toss = random.nextInt(2);
+                if( toss == P_NUM)
+                {
+                        System.out.println("You Won The Toss");
+                }
+                else
+                {
+                        System.out.println("Computer Won The Toss");
+                }
 
-	}
-	public void SymbolChoice()
-	{
+        }
 
-		if( toss == P_NUM)
-		{
-			System.out.print("Select Letter as your choice X and O : ");
-			Player_Symbol = scan.next().charAt(0);
-			System.out.println();
-			if( Player_Symbol == X )
-			{
-				Computer_Symbol = O;
-			}
-			else
-			{
-				Computer_Symbol = X;
-			}
-			}
-		else
-		{
-			int computer_random = random.nextInt(2);
-			if( computer_random == P_NUM )
-			{
-				Computer_Symbol = X;
-				Player_Symbol = O;
-			}
-			else
-			{
-				Computer_Symbol = O;
-				Player_Symbol = X;
-			}
-		}
-			System.out.println("Player Letter : "+Player_Symbol);
-			System.out.println("Computer Letter : "+Computer_Symbol);
-	}
+        public void SymbolChoice()
+        {
 
+                if( toss == P_NUM)
+                {
+                        System.out.print("Select Letter as your choice X and O : ");
+                        Player_Symbol = scan.next().charAt(0);
+                        System.out.println();
+                        if( Player_Symbol == X )
+                        {
+                                Computer_Symbol = O;
+                                System.out.println("Player Letter : "+Player_Symbol);
+                                System.out.println("Computer Letter : "+Computer_Symbol);
 
-	public void DisplayBoard()
-	{
-		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-		System.out.println("    |-----|-----|-----| ");
-		System.out.println("    |  "+board[ONE]+"  |  "+board[TWO]+"  |  "+board[THREE]+"  |");
-		System.out.println("    |-----|-----|-----| ");
-		System.out.println("    |  "+board[FOUR]+"  |  "+board[FIVE]+"  |  "+board[SIX]+"  |");
-		System.out.println("    |-----|-----|-----| ");
-		System.out.println("    |  "+board[SEVEN]+"  |  "+board[EIGHT]+"  |  "+board[NINE]+"  |");
-		System.out.println("    |-----|-----|-----| ");
-		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-	}
+                        }
+                        else if( Player_Symbol == O )
+                        {
+                                Computer_Symbol = X;
+                                System.out.println("Player Letter : "+Player_Symbol);
+                                System.out.println("Computer Letter : "+Computer_Symbol);
+                        }
+                        else
+                        {
+                                System.out.println("Not taken valid Symbol ! Try Again ! ");
+                                SymbolChoice();
+                        }
+                        }
+                else
+                {
+                        int computer_random = random.nextInt(2);
+                        if( computer_random == P_NUM )
+                        {
+                                Computer_Symbol = X;
+                                Player_Symbol = O;
+                        }
+                        else
+                        {
+                                Computer_Symbol = O;
+                                Player_Symbol = X;
+                        }
+                        System.out.println("Player Letter : "+Player_Symbol);
+                        System.out.println("Computer Letter : "+Computer_Symbol);
 
-	public void PlayerPlay()
-	{
+                }
+        }
+
+        public void PlayerPlay()
+        {
+                
+		System.out.println("++++++ Your Turn ++++++");
+                System.out.print("  Enter number to play  1 - 2 - 3 - 4 - 5 - 6 - 7 - 8 - 9 :");
+                position = scan.nextInt();
+
+                if(board[position] == '-' )
+                {
+                        board[position] = Player_Symbol;
+                        DisplayBoard();
+                        System.out.println();
+                }
+                else
+                {
+                        System.out.println("********** Position Already Occupied **********");
+                        PlayerPlay();
+                }
+                AllowComp = TWO;
 		CheckWon(P_NUM);
-		System.out.print("  Enter number to play  1 - 2 - 3 - 4 - 5 - 6 - 7 - 8 - 9 :");
-		position = scan.nextInt();
-		position--;
-		if(board[position] == '-' )
+        }
+
+	public void comprand()
+	{
+		int compposition = (random.nextInt(9)+1);
+		if( board[compposition] != Player_Symbol && board[compposition] != Computer_Symbol)
 		{
-			board[position] = Player_Symbol;
-			DisplayBoard();
-			System.out.println();
+			board[compposition] = Computer_Symbol;
 		}
 		else
 		{
-			System.out.println("********** Position Already Occupied **********");
-			PlayerPlay();
+		comprand();
 		}
 
 	}
 
 	public void ComputerPlay()
 	{
-		if( AllowComp == TWO )
+
+		System.out.println("++++++ Computer Turn ++++++");
+		try{
+		Thread.sleep(2000);
+		comprand();
+		DisplayBoard();
+               	System.out.println();
+		CheckWon(C_NUM);
+		}
+		catch(Exception e)
 		{
-//Play Computer on turn
+			 System.out.println(e);
 		}
 	}
+	
 
-	public void DisplayWinner(int win)
-	{
-		if(win == P_NUM)
-		{
-			System.out.println("************ Congrats You Won The Match ************ ");
-			System.exit(0);
-		}
-		else if(win == C_NUM)
-		{
-			System.out.println("************ You Lose The Match ************ ");
-			System.exit(0);
-		}
-		else
-		{
-			System.out.println("************ Match Tie ************ ");
-			System.exit(0);
-		}
-	}
-
-	public void CheckWon(int num)
+        public void CheckWon(int num)
         {
-                if( board[ONE] == board[TWO] && board[TWO] == board[THREE] && board[THREE] != '-' )
-                {
-                       DisplayWinner(num);
+
+	        for(int row = ONE; row < FOUR ; row++ )
+	        {
+//Winning in Rows
+	                if(board[row] == board[row+ONE] && board[row] == board[row+TWO] && board[row] != '-' )
+	                {
+	                        DisplayWinner(num);
+
+	                }
                 }
-                if( board[FOUR] == board[FIVE] && board[FIVE] == board[SIX] && board[SIX] != '-' )
-                {
-                         DisplayWinner(num);
-                }
-                if( board[SEVEN] == board[EIGHT] && board[EIGHT]  == board[NINE] && board[NINE] != '-' )
-                {
-			 DisplayWinner(num);
-                }
-                if( board[ONE] == board[FIVE] && board[FIVE]  == board[NINE] && board[NINE] != '-' )
-                {
-                        DisplayWinner(num);
-                }
-                if( board[THREE] == board[FIVE] && board[FIVE] == board[SEVEN] && board[SEVEN] != '-' )
-                {
-                         DisplayWinner(num);
+//Winning in Column
+	                for(int col = ONE; col < FOUR ; col++)
+	                {
+	                        if(board[col] == board[col + THREE] && board[col] == board[col + SIX] && board[col] != '-')
+	                        {
+	                                DisplayWinner(num);
+	                        }
+	                }
+//Winning in Diagonal
+			if((board[ONE] == board[FIVE] && board[FIVE] == board[NINE] && board[FIVE] != '-' ) || (board[THREE] == board[FIVE] && board[FIVE] == board[SEVEN] && board[FIVE] != '-'))
+	                {
+	                        DisplayWinner(num);
                 }
 
-                if( board[TWO] == board[FIVE] && board[FIVE] == board[EIGHT] && board[EIGHT] != '-' )
-                {
-                        DisplayWinner(num);
+        }
 
-                }
-                if( board[ONE] == board[FOUR] && board[FOUR] == board[SEVEN] && board[SEVEN] != '-' )
+        public void DisplayWinner(int win)
+        {
+                if(win == P_NUM)
                 {
-			 DisplayWinner(num);
+                        System.out.println("************ Congrats You Won The Match ************ ");
+                        System.exit(0);
                 }
-                if( board[THREE] == board[SIX] && board[SIX] == board[NINE] && board[NINE] != '-' )
+                else if(win == C_NUM)
                 {
-			 DisplayWinner(num);
+                        System.out.println("************ You Lose The Match ************ ");
+                        System.exit(0);
+                }
 
+        }
+	public void CheckTie()
+	{
+		if( tiecount == FIVE  )
+                {
+                        System.out.println("************ Match Tie ************ ");
+                        System.exit(0);
+		}
+	}
+
+
+
+
+        public void GamePlayed()
+        {
+		int count;
+                if( toss == P_NUM)
+                {
+                        for(count = START_POSITION ; count <= NINE; count++ )
+                        {
+                                PlayerPlay();
+				count++;
+                                ComputerPlay();
+				tiecount++;
+                        }
                 }
+                else
+                {
+                        for(count = START_POSITION ; count <= NINE ; count++ )
+                        {
+                                ComputerPlay();
+				count++;
+                                PlayerPlay();
+				tiecount++;
+                        }
+                }
+		CheckTie();
         }
 
 
-	public void GamePlayed()
-	{
-		if( toss == P_NUM)
-		{
-			for(count = START_POSITION ; count < END_POSITION ; count++ )
-			{
-				PlayerPlay();
-				ComputerPlay();
 
-			}
-		}
-		else
-		{
-			for(count = START_POSITION ; count < END_POSITION ; count++ )
-			{
-				ComputerPlay();
-				PlayerPlay();
-			}
-		}
-	}
+
+//Main Program
 
 	public static void main(String[] args)
 	{
-
 		TicTacToe Game = new TicTacToe();
 		Game.resetBoard();
+		Game.DisplayBoard();
 		Game.tossRandom();
 		Game.SymbolChoice();
-		Game.DisplayBoard();
 		Game.GamePlayed();
-	//	Game.DisplayWinner();
 	}
 }
 
